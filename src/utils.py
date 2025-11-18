fc517501c730866f9bef8cc3c1f6c0238d226c84
Lakshya Samraik <lakshyasamraik197@gmail.com>
2025-11-18 20:45:15 +0530

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,37 @@ def evaluate(model, loader, criterion, device):
             loss_sum += loss.item() * x.size(0)
             correct  += (out.argmax(1) == y).sum().item()
     return loss_sum / len(loader.dataset), correct / len(loader.dataset)
+def make_transforms(img_size):
+    train_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomRotation(15),
+        transforms.RandomHorizontalFlip(0.5),
+        transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.1),
+        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    eval_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    return train_transform, eval_transform, eval_transform
+
+def make_dataloaders(train_path, test_path, train_transform, validation_transform, test_transform, batch_size):
+    full_train_dataset = datasets.ImageFolder(root=train_path, transform=train_transform)
+    train_size = int(0.8 * len(full_train_dataset))
+    validation_size = len(full_train_dataset) - train_size
+    train_dataset, validation_dataset = random_split(full_train_dataset, [train_size, validation_size])
+
+    # Ensure correct transforms for split datasets
+    train_dataset.dataset.transform = train_transform
+    validation_dataset.dataset.transform = validation_transform
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, worker_init_fn=worker_init_fn)
+    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, num_workers=4, worker_init_fn=worker_init_fn)
+
+    test_dataset = datasets.ImageFolder(root=test_path, transform=test_transform)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, worker_init_fn=worker_init_fn)
+
+    return train_loader, validation_loader, test_loader
